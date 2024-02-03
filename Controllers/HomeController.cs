fc9249b7a7d3d2d717ed6049c9ce5b6ae82e0087
Federico Casadei <116793299+casaddei@users.noreply.github.com>
2024-02-03 09:38:22 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Prenota.Models;
+
+namespace Prenota.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    private static List <Utente> Utenti = new List<Utente>();
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Prenota()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Prenotato(Utente u)
+    {
+        Utenti.Add(u);
+        return View( Utenti );
+    }
+
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Utente.cs
@@ -0,0 +1,10 @@
+namespace Prenota.Models;
+
+public class Utente
+{
+     public string? Nome { get; set; }
+     public string? Cognome { get; set; }
+     public string? Tipo { get; set; }
+     public string? Email { get; set; }
+     
+}
