b3acaf185b56b18d1861078f5a57b16b52d6012a
Federico Casadei <116793299+casaddei@users.noreply.github.com>
2024-02-17 09:14:46 +0100

    Add files via upload

--- Models/Utente.cs
+++ Models/Utente.cs
@@ -2,9 +2,11 @@ namespace Prenota.Models;
 
 public class Utente
 {
+     public string? Id {get;set; }
      public string? Nome { get; set; }
      public string? Cognome { get; set; }
-     public string? Tipo { get; set; }
-     public string? Email { get; set; }
-     
+    public string? Email { get; set; }
+     public string? Città { get; set; }
+     public string? Tipo {get;set;}
+
 }
